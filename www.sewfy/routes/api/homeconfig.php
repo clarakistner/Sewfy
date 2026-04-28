f0d244664063623cb3b5d67fa4fa0d389d4d1291
Clara Kistner <clarabkistner@gmail.com>
2026-04-28 16:52:04 -0300

    routes homeconfig

--- www.sewfy/routes/api/homeconfig.php
+++ www.sewfy/routes/api/homeconfig.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeConfigController;
 
-// Rotas para HomeConfig
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum', 'impersonate'])->group(function () {
     Route::get('/home/config', [HomeConfigController::class, 'show']);
     Route::put('/home/config', [HomeConfigController::class, 'update']);
 });
